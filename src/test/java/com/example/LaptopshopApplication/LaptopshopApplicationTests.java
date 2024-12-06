ea5f94e4c3482e2cfe4294269c75451796b68294
TrienVo397 <s3907397@rmit.edu.vn>
2024-12-06 14:03:12 +0700

    fisrt commit

---
+++ src/test/java/com/example/LaptopshopApplication/LaptopshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LaptopshopApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaptopshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
